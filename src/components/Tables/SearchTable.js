cd801b21475edba8a16752bba405246089aa6e5b
Samuel-Jesus <samuel.jesan2018@gmail.com>
2023-07-16 22:26:50 -0300

    fix: install axios and import useEffect for component

--- src/components/Tables/SearchTable.js
+++ src/components/Tables/SearchTable.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import {Box, Button, TextField, Grid, Alert, AlertTitle, Typography } from '@mui/material';
 import SearchIcon from '@mui/icons-material/SearchOutlined';
 import dayjs from 'dayjs';
